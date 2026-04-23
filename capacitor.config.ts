176d888f891949d480b1e19c7086f26e3e6ff3c7
Praj <p.prajwal2004@gmail.com>
2026-04-23 21:26:37 +0530

    final edits

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.geolocation',
+  appName: 'Geolocation Based Attendance Tracking System',
+  webDir: 'dist'
+};
+
+export default config;
